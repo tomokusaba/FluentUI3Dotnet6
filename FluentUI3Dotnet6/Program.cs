b4ebd5407c7a513e41df7edf7b32b327863df51a
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2023-09-29 14:29:38 +0900

    順番

--- FluentUI3Dotnet6/Program.cs
+++ FluentUI3Dotnet6/Program.cs
@@ -6,10 +6,10 @@ using Microsoft.Fast.Components.FluentUI;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents(options =>
 {
     options.HostingModel = BlazorHostingModel.WebAssembly;
 });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
